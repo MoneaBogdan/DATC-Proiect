abd7bc3d688e371e17885ca783daf5916ee3ceca
Monea Bogdan <monea@Bogdan.0.2.15>
2018-11-27 17:30:27 +0200

    generare date (incomplet)

---
+++ generator_parcare/DatabaseContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generator_parcare
+{
+    class DatabaseContext
+    {
+    }
+}
